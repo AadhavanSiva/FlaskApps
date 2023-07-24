562849aadb66235214bdd595ed75ad6ef9a98370
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2023-07-24 18:15:41 -0400

    made static.py

---
+++ UserAuthentication/userAuthentication/routes/static.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template,url_for,redirect,request,flash, Blueprint
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/welcome')
+@login_required
+def welcome_user():
+    return render_template('welcome_user.html')
