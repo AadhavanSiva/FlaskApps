a93657e725abb8fd10224fa85b982dc14a5d28f4
Aadhvan Sivakumar <aadhavans2007@gmail.com>
2023-07-24 22:04:52 -0400

    Fixed routes for static resources.

--- UserAuthentication/userAuthentication/routes/static.py
+++ UserAuthentication/userAuthentication/routes/static.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template,url_for,redirect,request,flash, Blueprint
+from flask import render_template, Blueprint
+from flask_login import login_required
 
-@app.route('/')
+staticView = Blueprint('StaticRoutes',__name__, template_folder='/templates')
+
+
+@staticView.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/welcome')
+@staticView.route('/welcome')
 @login_required
 def welcome_user():
     return render_template('welcome_user.html')
+
